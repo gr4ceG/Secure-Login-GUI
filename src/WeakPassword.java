88b0881edb14166ddcbd0922fd994d11026ed459
gr4ceG <88872231+gr4ceG@users.noreply.github.com>
2022-05-08 20:19:35 -0400

    first commit

---
+++ src/WeakPassword.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Author: Grace Guo 
+ * Class: ICS4UE-51
+ * Program: Secure Login
+ */
+public class WeakPassword extends Exception{
+    
+    /**
+     * Throws exception for bad password
+     */
+    public WeakPassword(){
+        
+    }
+    
+}
